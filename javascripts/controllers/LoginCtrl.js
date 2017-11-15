f0f92b93998a8598f9d8e583bf7a78c7deee54cc
Gina Antonini <ginamantonini@gmail.com>
2017-11-15 17:35:45 -0600

    I consoled logged that's all

--- javascripts/controllers/LoginCtrl.js
+++ javascripts/controllers/LoginCtrl.js
@@ -4,6 +4,7 @@ app.controller("LoginCtrl", function($rootScope, $scope, AuthService){
     $scope.authenticate = () => {
         AuthService.authenticateGoogle().then((result) => {
             $rootScope.uid = result.user.uid;
+            console.log($rootScope);
             }).catch((err) => {
                 console.log("error in authenticateGoogle", err);
             });
