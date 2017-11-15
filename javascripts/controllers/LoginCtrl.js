662e87786d84477af56bfb39e1b67444bb9ce5aa
Gina Antonini <ginamantonini@gmail.com>
2017-11-15 00:30:56 -0600

    added contacts json file

--- javascripts/controllers/LoginCtrl.js
+++ javascripts/controllers/LoginCtrl.js
@@ -1,8 +1,11 @@
 "use strict";
 
-
-
-app.controller("LoginCtrl", function($scope){
-    console.log("Hello, Login");
-    $scope.controller = "LoginCtrl";
+app.controller("LoginCtrl", function($rootScope, $scope, AuthService){
+    $scope.authenticate = () => {
+        AuthService.authenticateGoogle().then((result) => {
+            $rootScope.uid = result.user.uid;
+            }).catch((err) => {
+                console.log("error in authenticateGoogle", err);
+            });
+    };
 });
