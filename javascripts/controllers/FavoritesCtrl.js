e96a6746fb95608d99ad8bf8bc41067adb1fc530
Gina Antonini <ginamantonini@gmail.com>
2017-11-18 13:54:40 -0600

    validation working

--- javascripts/controllers/FavoritesCtrl.js
+++ javascripts/controllers/FavoritesCtrl.js
@@ -1,5 +1,13 @@
 "use strict";
 
 app.controller("FavoritesCtrl", function($scope){
-    $scope.controller = "ContactsFavoritesCtrl";
+    $scope.starChange = (event, contact) => {
+        if (event.favorite){
+            contact.rating = event.favorite;
+            let updatedContact = NewContactService.
+        }
+    }
 });
+
+
+
