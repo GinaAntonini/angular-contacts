e2c0a29810d9aa4d136f9e86ec64b5c026543695
Gina Antonini <ginamantonini@gmail.com>
2017-11-18 13:30:57 -0600

    added filled stars to favorited contacts

--- javascripts/services/NewContactService.js
+++ javascripts/services/NewContactService.js
@@ -25,9 +25,30 @@ app.service("NewContactService", function($http, $q, FIREBASE_CONFIG){
 
 	const deleteContact = (contactId) => {
 		return $http.delete(`${FIREBASE_CONFIG.databaseURL}/contacts/${contactId}.json`);
+  };
+  
+  const updateContact = (contactId, contact) => {
+		return $http.put(`${FIREBASE_CONFIG.databaseURL}/contacts/${contactId}.json`), JSON.stringify(contact);
 	};
 
+  const createContactObject = (contact) => {
+    return {
+      "firstName": contact.firstName,
+      "lastName": contact.lastName,
+      "nickname": contact.nickname,
+      "streetAddress": contact.streetAddress,
+      "city": contact.city,
+      "state": contact.state,
+      "zip": contact.zip,
+      "phoneNumber": contact.phoneNumber,
+      "email": contact.email,
+      "birthday": contact.birthday,
+      "favorite": contact.favorite,
+      "uid": contact.uid,
+    };
+  };
 
-	return {getContactsFromFirebase, postNewContact, deleteContact};
+
+	return {updateContact, getContactsFromFirebase, postNewContact, deleteContact};
 });
 
