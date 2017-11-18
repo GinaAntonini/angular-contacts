eda4de533c6e23175461db528ef202d3dd66e66d
Gina Antonini <ginamantonini@gmail.com>
2017-11-18 08:19:31 -0600

    contacts are deleting

--- javascripts/controllers/ViewCtrl.js
+++ javascripts/controllers/ViewCtrl.js
@@ -4,10 +4,18 @@ app.controller("ViewCtrl", function($rootScope, $scope, NewContactService){
 	const getContacts = () => {
 		$scope.newContact = [];
     		NewContactService.getContactsFromFirebase($rootScope.uid).then((results) => {
-    		$scope.contacts = results.data;
+    		$scope.contacts = results;
     	}).catch((err) => {
     	console.log("error in getContacts", err);
    	});
    };
    getContacts();
+
+	$scope.deleteContactFromFirebase = (contactId) => {
+		NewContactService.deleteContact(contactId).then((result) =>{
+			getContacts();
+		}).catch((err) =>{
+		console.log("error in deleteContactFromFirebase", err);
+    });
+	};
 });
--- javascripts/services/NewContactService.js
+++ javascripts/services/NewContactService.js
@@ -3,8 +3,21 @@
 app.service("NewContactService", function($http, $q, FIREBASE_CONFIG){
 
 	const getContactsFromFirebase = (userUid) => {
-		return $http.get(`${FIREBASE_CONFIG.databaseURL}/contacts.json?orderBy="uid"&equalTo="${userUid}"`);
-	};
+		let contacts = [];
+    return $q((resolve, reject) => {
+      $http.get(`${FIREBASE_CONFIG.databaseURL}/contacts.json?orderBy="uid"&equalTo="${userUid}"`).then((results) => {
+        let fbContacts = results.data;
+        Object.keys(fbContacts).forEach((key) => {
+          fbContacts[key].id = key;
+            contacts.push(fbContacts[key]);
+        });
+        resolve(contacts);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  };
+
 
 	const postNewContact = (newContact) => {
 		return $http.post(`${FIREBASE_CONFIG.databaseURL}/contacts.json`, JSON.stringify(newContact));
