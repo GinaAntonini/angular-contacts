1ccc23370542c0131abc2cac7255750cb3ce31cc
Gina Antonini <ginamantonini@gmail.com>
2017-11-18 12:23:33 -0600

    added logout function to AuthService, added event listener in app.config

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -1,7 +1,28 @@
 'use strict';
 
+let isAuth = (AuthService) => new Promise ((resolve, reject) => {
+  if(AuthService.isAuthenticated()){
+    resolve();
+  } else {
+    reject();
+  }
+});
+
 app.run(function($location, $rootScope, FIREBASE_CONFIG, AuthService){
   firebase.initializeApp(FIREBASE_CONFIG);
+
+  $rootScope.$on('$routeChangeStart', function(event, currRoute, prevRoute) {
+    var logged = AuthService.isAuthenticated();
+    var appTo;
+    if (currRoute.originalPath) {
+      appTo = currRoute.originalPath.indexOf('/login') !== -1;
+    }
+    if (!appTo && !logged) {
+      event.preventDefault();
+      $location.path('/login');
+    }
+  });
+
 });
 
 app.config(function($routeProvider){
@@ -9,18 +30,21 @@ app.config(function($routeProvider){
     .when("/contacts/favorites", {
       templateUrl: 'partials/favorites.html',
       controller: 'FavoritesCtrl',
+      resolve: {isAuth}
     })
     .when("/contacts/new", {
       templateUrl: 'partials/new.html',
       controller: 'NewCtrl',
+      resolve: {isAuth}
     })
     .when("/contacts/view", {
       templateUrl: 'partials/view.html',
       controller: 'ViewCtrl',
+      resolve: {isAuth}
     })
     .when("/login", {
       templateUrl: 'partials/login.html',
-      controller: 'LoginCtrl',
+      controller: 'LoginCtrl'
     })
     .otherwise('/login');
 });
--- javascripts/controllers/NavCtrl.js
+++ javascripts/controllers/NavCtrl.js
@@ -1,10 +1,9 @@
 'use strict';
  
-app.controller("NavCtrl", function($location, $rootScope, $scope, $window, AuthService){
+app.controller("NavCtrl", function($location, $rootScope, $scope, $window){
 	$scope.logoutUser = () => {
     	delete $rootScope.uid;
     	$window.localStorage.clear();
-    	AuthService.logout();
     	$location.path('/login');
   	};
 });
